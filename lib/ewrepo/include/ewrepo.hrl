b141592e80060162f237eb6e98acb6ee5929bafa
Martin Logan <martinjlogan@erlware.org>
2008-01-07 01:06:43 -0600

    added package extension regexp to ewrepo.hrl

--- lib/ewrepo/include/ewrepo.hrl
+++ lib/ewrepo/include/ewrepo.hrl
@@ -1,4 +1,5 @@
-
 -define(PACKAGE_NAME_REGEXP, "[a-z]+[a-zA-Z0-9_]*").
 -define(PACKAGE_VSN_REGEXP, "[a-zA-Z0-9_]+([.-][a-zA-Z0-9_]+)*").
--define(PACKAGE_NAME_AND_VSN_REGEXP, lists:flatten(["^", ?PACKAGE_NAME_REGEXP, "-", ?PACKAGE_VSN_REGEXP, "(\.tar\.gz)*$"])).
+-define(PACKAGE_EXT_REGEXP, "(\.tar\.gz|\.tgz|\.epkg)").
+-define(PACKAGE_NAME_AND_VSN_REGEXP, 
+	lists:flatten(["^", ?PACKAGE_NAME_REGEXP, "-", ?PACKAGE_VSN_REGEXP, ?PACKAGE_EXT_REGEXP, "*$"])).
