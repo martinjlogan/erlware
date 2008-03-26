11f797299e1b3680d12a8029d7c52751fd6cf0e8
Martin Logan <martinjlogan@erlware.org>
2008-03-25 20:20:04 -0500

    added prime number generation function to eg_math for "cryptographic"
    cryptographic is the app that will house the RSA implementation used by
    Faxien and Portius. This app can later the the home for more advanced pure
    erlang crypto functions.

---
+++ lib/cryptographic/src/cg_math.erl
@@ -0,0 +1,69 @@
+%%%-------------------------------------------------------------------
+%%% @author Martin Logan <martinjlogan@sixfoe>
+%%% @copyright (C) 2008, Martin Logan
+%%% @doc
+%%%  Mathematical functions needed for cryptograpic functions but not supplied by Erlang standard libs.
+%%% @end
+%%% Created : 25 Mar 2008 by Martin Logan <martinjlogan@sixfoe>
+%%%-------------------------------------------------------------------
+-module(cg_math).
+
+%% API
+-export([
+	 prime/1, 
+	 floor/1
+	]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc Returns all prime numbers from 1 to N
+%% @spec primes(N) -> [integer()]
+%% @end
+%%--------------------------------------------------------------------
+prime(N) when N < 2 ->
+    [];
+prime(2) ->
+    [2];
+prime(N) ->
+    S         = lists:seq(3, N, 2),
+    MRoot     = math:sqrt(N),
+    Half      = length(S),
+    primeit(S, MRoot, Half).
+
+primeit(S, MRoot, Half) ->
+    primeit(3, 1, array:from_list(S), MRoot, Half).
+
+primeit(M, _I, S, MRoot, _Half) when M > MRoot ->
+    [2|array:sparse_to_list(S)];
+primeit(M, I, S, MRoot, Half) ->
+    case array:get(I, S) of
+	Undef when Undef == undefined; Undef == 0 ->
+	    primeit(2 * I + 4, I + 1, S, MRoot, Half);
+	_Int ->
+	    J = floor((M * M - 3) / 2),
+	    NS = vacumeit(array:set(J, undefined, S), M, J, Half),
+	    primeit(2 * I + 4, I + 1, NS, MRoot, Half)
+    end.
+	    
+vacumeit(S, _M, J, Half) when J >= Half ->
+    S;
+vacumeit(S, M, J, Half) ->
+    vacumeit(array:set(J, undefined, S), M, J + M, Half).
+
+%%--------------------------------------------------------------------
+%% @doc Returns the highest integer less than or equal to the number N.
+%% @spec primes(N) -> [integer()]
+%% @end
+%%--------------------------------------------------------------------
+floor(N) ->
+    case round(N) of
+	RN when RN =< N -> RN;
+	RN -> RN - 1
+    end.
+	    
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
