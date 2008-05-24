527aca18281beadfdc7fb27b806aeb94a7467f5f
Eric Merritt <cyberlync@gmail.com>
2008-05-23 20:14:39 -0700

    throw an error on bad tarballs
    If the tarball cannot be untarred we should throw an error instead of exiting silently.

--- lib/ewrepo/src/ewr_fetch.erl
+++ lib/ewrepo/src/ewr_fetch.erl
@@ -1,25 +1,25 @@
 %%%-------------------------------------------------------------------
 %%% Copyright (c) 2006 Eric Merritt, Martin Logan
 %%%
-%%% Permission is hereby granted, free of charge, to any 
-%%% person obtaining a copy of this software and associated 
-%%% documentation files (the "Software"), to deal in the 
-%%% Software without restriction, including without limitation 
+%%% Permission is hereby granted, free of charge, to any
+%%% person obtaining a copy of this software and associated
+%%% documentation files (the "Software"), to deal in the
+%%% Software without restriction, including without limitation
 %%% the rights to use, copy, modify, merge, publish, distribute,
-%%% sublicense, and/or sell copies of the Software, and to permit 
-%%% persons to whom the Software is furnished to do so, subject to 
+%%% sublicense, and/or sell copies of the Software, and to permit
+%%% persons to whom the Software is furnished to do so, subject to
 %%% the following conditions:
 %%%
-%%% The above copyright notice and this permission notice shall 
+%%% The above copyright notice and this permission notice shall
 %%% be included in all copies or substantial portions of the Software.
 %%%
 %%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-%%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
-%%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-%%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+%%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+%%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+%%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
 %%% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 %%% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 %%% OTHER DEALINGS IN THE SOFTWARE.
 %%%-------------------------------------------------------------------
 %%% @author Eric Merritt,  Martin Logan
@@ -29,8 +29,8 @@
 %%%   into the local repository.
 %%%
 %%% Types:
-%%%  @type repo() = string(). Contains address and repo designation. 
-%%%   Example: http://www.erlware.org/stable   
+%%%  @type repo() = string(). Contains address and repo designation.
+%%%   Example: http://www.erlware.org/stable
 %%%  @type timeout() = integer() | infinity. Timeouts are specified in milliseconds.
 %%%
 %%% @end
@@ -39,21 +39,21 @@
 
 %% API
 -export([
-	 fetch_release_package/6, 
-	 fetch_release_package/5, 
-	 fetch_release_package/4, 
+	 fetch_release_package/6,
+	 fetch_release_package/5,
+	 fetch_release_package/4,
          fetch_release_packages/5,
          fetch_release_packages/4,
          fetch_release_packages/3,
-	 fetch_binary_package/6, 
-	 fetch_binary_package/5, 
-	 fetch_binary_package/4, 
+	 fetch_binary_package/6,
+	 fetch_binary_package/5,
+	 fetch_binary_package/4,
          fetch_binary_packages/5,
          fetch_binary_packages/4,
          fetch_binary_packages/3,
-         fetch_source_package/6, 
-         fetch_source_package/5, 
-         fetch_source_package/4, 
+         fetch_source_package/6,
+         fetch_source_package/5,
+         fetch_source_package/4,
          fetch_source_packages/5,
          fetch_source_packages/4,
          fetch_source_packages/3,
@@ -70,15 +70,15 @@
 %%====================================================================
 
 %%-------------------------------------------------------------------
-%% @spec fetch_source_package(Repos::list(), ErtsVsn, Package::string(), Version::string(), To::string(), Timeout::timeout()) -> 
+%% @spec fetch_source_package(Repos::list(), ErtsVsn, Package::string(), Version::string(), To::string(), Timeout::timeout()) ->
 %%                            ok | {error, Reason}
-%% where 
+%% where
 %%  Timeout = integer() | infinity
 %% @doc
-%%  Fetch the single source package (Package and Version) into the 
+%%  Fetch the single source package (Package and Version) into the
 %%  location specified (To) from the repositories (Repos).
 %%  usage
-%%  
+%%
 %%  ``ewr_fetch:fetch_source_package(["http://urltorepo/stable"], "5.5.5", "maypackage", "1.2.3", "./target").''
 %% @end
 %%-------------------------------------------------------------------
@@ -98,10 +98,10 @@ fetch_source_package(Repos, Package, Version, To) ->
     fetch_source_package(Repos, ErtsVsn, Package, Version, To, ?FETCH_TIMEOUT).
 
 %%-------------------------------------------------------------------
-%% @spec fetch_source_packages(Repos::list(), ErtsVsn::string(), PackageList::list(), To::string(), Timeout::timeout()) -> 
+%% @spec fetch_source_packages(Repos::list(), ErtsVsn::string(), PackageList::list(), To::string(), Timeout::timeout()) ->
 %%                             ok | {error, Reason}
 %% @doc
-%%  Fetch all packages in the list (PackageList {Name, Version}), into 
+%%  Fetch all packages in the list (PackageList {Name, Version}), into
 %%  the location (To) from the repositories (Repos).
 %%
 %%  ``ewr_fetch:fetch_source_packages(["http://urltorepo/stable"],
@@ -119,29 +119,29 @@ fetch_source_packages(_Repos, _ErtsVsn, [], _To, _Timeout) ->
     ok.
 
 %% @spec fetch_source_packages(Repos::list(), PackageList::list(), To::string(), Timeout::integer()) -> ok | {error, Reason}
-%% @equiv fetch_source_packages(Repos, ErtsVsn, PackageList, To, Timeout) 
+%% @equiv fetch_source_packages(Repos, ErtsVsn, PackageList, To, Timeout)
 fetch_source_packages(Repos, PackageList, To, Timeout) ->
     ErtsVsn = ewr_util:erts_version(),
     fetch_source_packages(Repos, ErtsVsn, PackageList, To, Timeout).
 
 %% @spec fetch_source_packages(Repos::list(), PackageList::list(), To::string()) -> ok | {error, Reason}
-%% @equiv fetch_source_packages(Repos, PackageList, To, Timeout) 
+%% @equiv fetch_source_packages(Repos, PackageList, To, Timeout)
 fetch_source_packages(Repos, PackageList, To) ->
     fetch_source_packages(Repos, PackageList, To, ?FETCH_TIMEOUT).
 
 
 %%-------------------------------------------------------------------
-%% @spec fetch_binary_package(Repos::list(), ErtsVsn, Package::string(), Version::string(), To::string(), Timeout::timeout()) -> 
+%% @spec fetch_binary_package(Repos::list(), ErtsVsn, Package::string(), Version::string(), To::string(), Timeout::timeout()) ->
 %%                            ok | {error, Reason}
 %% @doc
-%%  Fetch the single binary package (Package and Version) into the 
+%%  Fetch the single binary package (Package and Version) into the
 %%  location specified (To) from the repositories (Repos).
 %%
 %%  ``ewr_fetch:fetch_binary_package(["http://urltorepo/stable"],
 %%                                   "5.5.5",
 %%                                   "mypackage",
 %%                                   "1.2.3",
-%%                                   "./target", 
+%%                                   "./target",
 %%                                   10000).''
 %% @end
 %%-------------------------------------------------------------------
@@ -161,10 +161,10 @@ fetch_binary_package(Repos, Package, Version, To) ->
     fetch_binary_package(Repos, Package, Version, To, ?FETCH_TIMEOUT).
 
 %%-------------------------------------------------------------------
-%% @spec fetch_binary_packages(Repos::list(), ErtsVsn::string(), PackageList::list(), To::string(), Timeout::timeout()) -> 
+%% @spec fetch_binary_packages(Repos::list(), ErtsVsn::string(), PackageList::list(), To::string(), Timeout::timeout()) ->
 %%                             ok | {error, Reason}
 %% @doc
-%%  Fetch all packages in the list (PackageList {Name, Version}), into 
+%%  Fetch all packages in the list (PackageList {Name, Version}), into
 %%  the location (To) from the repositories (Repos).
 %%
 %%  ``ewr_fetch:fetch_binary_packages(["http://urltorepo/stable"],
@@ -193,10 +193,10 @@ fetch_binary_packages(Repos, PackageList, To) ->
     fetch_binary_packages(Repos, PackageList, To, ?FETCH_TIMEOUT).
 
 %%-------------------------------------------------------------------
-%% @spec fetch_release_package(Repos::list(), ErtsVsn::string(), Package::string(), Version::string(), 
+%% @spec fetch_release_package(Repos::list(), ErtsVsn::string(), Package::string(), Version::string(),
 %%                             To::string(), Timeout::timeout()) -> ok | {error, Reason}
 %% @doc
-%%  Fetch the single release package (Package and Version) into the 
+%%  Fetch the single release package (Package and Version) into the
 %%  location specified (To) from the repositories (Repos).
 %%
 %%  ``ewr_fetch:fetch_release_package(["http://urltorepo/stable"],
@@ -211,7 +211,7 @@ fetch_release_package(Repos, ErtsVsn, Package, Version, To, Timeout) ->
     SysInfo = ewr_util:system_info(),
     fetch_package(Repos, ErtsVsn, Package, Version, To, [SysInfo, "Generic"], releases, Timeout).
 
-%% @spec fetch_release_package(Repos::list(), ErtsVsn::string(), Package::string(), Version::string(), To::string()) -> 
+%% @spec fetch_release_package(Repos::list(), ErtsVsn::string(), Package::string(), Version::string(), To::string()) ->
 %%                             ok | {error, Reason}
 %% @equiv fetch_release_package(Repos, ErtsVsn, Package, Version, To, Timeout)
 fetch_release_package(Repos, ErtsVsn, Package, Version, To) ->
@@ -224,10 +224,10 @@ fetch_release_package(Repos, Package, Version, To) ->
     fetch_release_package(Repos, ErtsVsn, Package, Version, To).
 
 %%-------------------------------------------------------------------
-%% @spec fetch_release_packages(Repos::list(), ErtsVsn::string(), PackageList::list(), To::string(), Timeout::timeout()) -> 
+%% @spec fetch_release_packages(Repos::list(), ErtsVsn::string(), PackageList::list(), To::string(), Timeout::timeout()) ->
 %%                              ok | {error, Reason}
 %% @doc
-%%  Fetch all packages in the list (PackageList {Name, Version}), into 
+%%  Fetch all packages in the list (PackageList {Name, Version}), into
 %%  the location (To) from the repositories (Repos).
 %%
 %%  ``ewr_fetch:fetch_release_packages(["http://urltorepo/stable"],
@@ -257,9 +257,9 @@ fetch_release_packages(Repos, PackageList, To) ->
 
 %%-------------------------------------------------------------------
 %% @doc
-%%   Fetch the specified package from the repos looking into the 
+%%   Fetch the specified package from the repos looking into the
 %%   areas supplied by Areas.
-%% 
+%%
 %% @spec fetch_erts_package(Repos, Version, To, Timeout) -> ok | {error, Reason}
 %% where
 %%  Repos = [string()]
@@ -273,7 +273,7 @@ fetch_erts_package(Repos, Version, To, Timeout) ->
         false ->
             filelib:ensure_dir(filename:join([To, "tmp"])),
 	    SysInfo = ewr_util:system_info(),
-            fetch_from_repos(Repos, [filename:dirname(ewr_repo_paths:erts_package_suffix(Version, SysInfo))], 
+            fetch_from_repos(Repos, [filename:dirname(ewr_repo_paths:erts_package_suffix(Version, SysInfo))],
 			     "/erts.tar.gz", To, Timeout);
         true ->
             ok
@@ -285,17 +285,17 @@ fetch_erts_package(Repos, Version, To) ->
     fetch_erts_package(Repos, Version, To, ?FETCH_TIMEOUT).
 
 %%-------------------------------------------------------------------
-%% @spec fetch_package(Repos, ErtsVsn, Package, Version, To, Areas, Side::atom(), Timeout::timeout()) -> ok | 
+%% @spec fetch_package(Repos, ErtsVsn, Package, Version, To, Areas, Side::atom(), Timeout::timeout()) -> ok |
 %%        {error, Reason}
 %% @doc
-%%   Fetch the specified package from the repos looking into the 
+%%   Fetch the specified package from the repos looking into the
 %%   areas supplied by Areas.
 %% @end
 %%-------------------------------------------------------------------
 fetch_package(Repos, ErtsVsn, Package, Version, To, Areas, Side, Timeout) when is_atom(Package) ->
     fetch_package(Repos, ErtsVsn, atom_to_list(Package), Version, To, Areas, Side, Timeout);
 fetch_package(Repos, ErtsVsn, Package, Version, To, Areas, Side, Timeout) ->
-    error_logger:info_msg("ewr_fetch:fetch_package(~p, ~p, ~p, ~p, ~p, ~p, ~p, ~p)~n", 
+    error_logger:info_msg("ewr_fetch:fetch_package(~p, ~p, ~p, ~p, ~p, ~p, ~p, ~p)~n",
 			  [Repos, ErtsVsn, Package, Version, To, Areas, Side, Timeout]),
     case is_package_local(To, Package, Version) of
         false ->
@@ -319,8 +319,8 @@ fetch_package(Repos, Package, Version, To, Areas, Side, Timeout) ->
 
 %%--------------------------------------------------------------------
 %% @spec is_package_local(To, Package, Version) -> true | false
-%% 
-%% @doc 
+%%
+%% @doc
 %%  Check to see if the package exists.
 %% @end
 %%--------------------------------------------------------------------
@@ -337,7 +337,7 @@ is_package_local(To, Package, Version) ->
 %%-------------------------------------------------------------------
 %% @spec fetch_from_repos(Repos, PossibleLocations, TarName, To, Timeout) -> ok
 %% @doc
-%%  Actually try to fetch the TarName tar file from the specified 
+%%  Actually try to fetch the TarName tar file from the specified
 %%  repository.
 %% @end
 %% @private
@@ -351,9 +351,9 @@ fetch_from_repos([Repo | Rest], PosLocations, TarName, To, Timeout) ->
             fetch_from_repos(Rest, PosLocations, TarName, To, Timeout)
     end;
 fetch_from_repos([], _PosLocations, TarName, _To, _Timeout) ->
-    throw({unable_to_pull_from_repos, 
+    throw({unable_to_pull_from_repos,
            lists:flatten(["Couldn't get ", TarName, " from repos"])}).
-   
+
 
 fetch_from_repo(Repo, [Uri | T], TarName, To, Timeout) ->
     Suffix = lists:flatten([Uri, TarName]),
@@ -364,9 +364,9 @@ fetch_from_repo(Repo, [Uri | T], TarName, To, Timeout) ->
             fetch_from_repo(Repo, T, TarName, To, Timeout)
     end;
 fetch_from_repo(_Repo, [], TarName, _To, _Timeout) ->
-    throw({not_available, lists:flatten([TarName, 
+    throw({not_available, lists:flatten([TarName,
                                          " not available in repository"])}).
-    
+
 
 
 %%-------------------------------------------------------------------
@@ -410,14 +410,17 @@ write_data(Data, To) ->
 %%-------------------------------------------------------------------
 %% @spec handle_tar_file(To, ActualTo) -> ok
 %% @doc
-%%  Put the tar file in the correct location, the directory 
+%%  Put the tar file in the correct location, the directory
 %%  specified by To and the file by ActualTo
 %% @end
 %% @private
 %%-------------------------------------------------------------------
 handle_tar_file(To, ActualTo) ->
     Cmd = lists:flatten(["tar -xzf ", ewr_util:handle_cygwin_path(ActualTo), " -C ", ewr_util:handle_cygwin_path(To)]),
-    os:cmd(Cmd),
+    case os:cmd(Cmd) of
+        [] -> ok;
+        Error -> throw({error, {unable_to_untar, ActualTo}, Error})
+    end,
     file:delete(ewr_util:handle_cygwin_path(ActualTo)),
     ok.
 
