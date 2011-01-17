7344d0f1fa1d60b254374d433efcefdce6d3ab6b
Martin J. Logan <martinjlogan@erlware.org>
2011-01-16 22:30:38 -0600

    fixed use of record tuple
    Signed-off-by: Jordan Wilberding <jwilberding@gmail.com>

--- lib/ewlib/src/ewl_file.erl
+++ lib/ewlib/src/ewl_file.erl
@@ -1,27 +1,4 @@
 %%%-------------------------------------------------------------------
-%%% Copyright (c) 2006 Erlware
-%%%
-%%% Permission is hereby granted, free of charge, to any 
-%%% person obtaining a copy of this software and associated 
-%%% documentation files (the "Software"), to deal in the 
-%%% Software without restriction, including without limitation 
-%%% the rights to use, copy, modify, merge, publish, distribute,
-%%% sublicense, and/or sell copies of the Software, and to permit 
-%%% persons to whom the Software is furnished to do so, subject to 
-%%% the following conditions:
-%%%
-%%% The above copyright notice and this permission notice shall 
-%%% be included in all copies or substantial portions of the Software.
-%%%
-%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-%%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
-%%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-%%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-%%% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-%%% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
-%%% OTHER DEALINGS IN THE SOFTWARE.
-%%%-------------------------------------------------------------------
 %%% @author  Martin J. Logan 
 %%%
 %%% @doc
@@ -30,9 +7,8 @@
 %%%-------------------------------------------------------------------
 -module(ewl_file).
 
-%%--------------------------------------------------------------------
-%% External exports
-%%--------------------------------------------------------------------
+-include_lib("kernel/include/file.hrl").
+
 -export([
 	 join_paths/2,
 	 find/2,
@@ -57,6 +33,7 @@
 %% Include Files
 %%--------------------------------------------------------------------
 -include("eunit.hrl").
+-include_lib("kernel/include/file.hrl").
 
 %%====================================================================
 %% External functions
@@ -104,10 +81,8 @@ remove_recursive(Path, Options) ->
 -spec is_symlink(Path::string()) -> bool().
 is_symlink(Path) ->
     case catch file:read_link_info(Path) of
-	{ok, LinkInfo} ->
-	    % XXX Exploiting the structure of a record, tisk, tisk,
-	    % should probably include the proper .hrl file.
-	    symlink == element(3, LinkInfo);
+	{ok, #file_info{type = symlink}} ->
+	    true;
 	_ ->
 	    false
     end.
