0e759ab9992112cb71c1f30c5c888baa0889da05
Martin J. Logan <martinjlogan@erlware.org>
2011-02-12 17:48:58 -0600

    brought plists up to API spec and fixed error with filter only returning bools

--- lib/ewlib/src/ewl_plists.erl
+++ lib/ewlib/src/ewl_plists.erl
@@ -1,20 +1,15 @@
 %%%-------------------------------------------------------------------
-%%% @author Eric Merritt 
 %%% @doc
 %%% simple parrallel map. Originally provided by Joe Armstrong
 %%% on the erlang questions mailing list.
-%%%
-%%% Addition by Tristan: Added timeout to map and added filter
-%%% with timeout.
-%%%
-%%% Addition by Martin Logan: Added exception handling
-%%%
 %%% @end
 %%%-------------------------------------------------------------------
 -module(ewl_plists).
 
 -export([map/2,
          map/3,
+         ftmap/2,
+         ftmap/3,
          filter/2,
          filter/3]).
 
@@ -22,27 +17,48 @@
 %%=============================================================================
 %% Public API
 %%=============================================================================
-%%--------------------------------------------------------------------
+
 %% @doc Takes a function and produces a list of the result of the function
 %%      applied to each element of the argument list. A timeout is optional.
-%%      In the event of a timeout the resultant element is the atom timeout.
-%%--------------------------------------------------------------------
+%%      In the event of a timeout or an exception the entire map will fail
+%%      with an excption with class throw.
 -spec map(fun(), [any()]) -> [any()].
 map(Fun, List) ->
     map(Fun, List, infinity).
--spec map(fun(), [any()], integer()) -> [any()].
+
+-spec map(fun(), [any()], non_neg_integer()) -> [any()].
 map(Fun, List, Timeout) ->
     run_list_fun_in_parallel(map, Fun, List, Timeout).
 
-%%--------------------------------------------------------------------
+%% @doc Takes a function and produces a list of the result of the function
+%%      applied to each element of the argument list. A timeout is optional.
+%%      This function differes from regular map in that it is fault tolerant.
+%%      If a timeout or an exception occurs while processing an element in
+%%      the input list the ftmap operation will continue to function. Timeouts
+%%      and exceptions will be reflected in the output of this function.
+%%      All application level results are wrapped in a tuple with the tag
+%%      'value'. Exceptions will come through as they are and timeouts will
+%%      return as the atom timeout.
+%%      This is useful when the ftmap is being used for side effects. 
+%% <pre>
+%% 2> ftmap(fun(N) -> factorial(N) end, [1, 2, 1000000, "not num"], 100)
+%% [{value, 1}, {value, 2}, timeout, {badmatch, ...}]
+%% </pre>
+-spec ftmap(fun(), [any()]) -> [{value, any()} | any()].
+ftmap(Fun, List) ->
+    ftmap(Fun, List, infinity).
+
+-spec ftmap(fun(), [any()], non_neg_integer()) -> [{value, any()} | any()].
+ftmap(Fun, List, Timeout) ->
+    run_list_fun_in_parallel(ftmap, Fun, List, Timeout).
+
 %% @doc Returns a list of the elements in the supplied list which
 %%      the function Fun returns true. A timeout is optional. In the
-%%      event of a timeout the element is treated as if it did not
-%%      return true, and is thus not in the resultant list.
-%%--------------------------------------------------------------------
+%%      event of a timeout the filter operation fails.
 -spec filter(fun(), [any()]) -> [any()].
 filter(Fun, List) ->
     filter(Fun, List, infinity).
+
 -spec filter(fun(), [any()], integer()) -> [any()].
 filter(Fun, List, Timeout) ->
     run_list_fun_in_parallel(filter, Fun, List, Timeout).
@@ -53,18 +69,19 @@ filter(Fun, List, Timeout) ->
 -spec run_list_fun_in_parallel(atom(), fun(), [any()], integer()) -> [any()].
 run_list_fun_in_parallel(ListFun, Fun, List, Timeout) ->
     LocalPid = self(),
-    Pids = lists:map(fun(I) ->
-                           proc_lib:spawn(fun() ->
-                                                  wait(LocalPid, Fun, I, Timeout)
-                                          end)
+    Pids = lists:map(fun(E) ->
+			     Pid = proc_lib:spawn(fun() ->
+							  wait(LocalPid, Fun, E, Timeout)
+						  end),
+			     {Pid, E}
                    end, List),
     gather(ListFun, Pids).
 
 -spec wait(pid(), fun(), any(), integer()) -> any().
-wait(Parent, Fun, I, Timeout) ->
+wait(Parent, Fun, E, Timeout) ->
     WaitPid = self(),
     Child = spawn(fun() ->
-                          do_f(WaitPid, Fun, I)
+                          do_f(WaitPid, Fun, E)
                   end),
 
     wait(Parent, Child, Timeout).
@@ -80,62 +97,77 @@ wait(Parent, Child, Timeout) ->
     end.
 
 -spec gather(atom(), [any()]) -> [any()].
-gather(map, PidList) ->
-    map_gather(PidList);
-gather(filter, PidList) ->
-    filter_gather(PidList).
+gather(map, PidElementList) ->
+    map_gather(PidElementList);
+gather(ftmap, PidElementList) ->
+    ftmap_gather(PidElementList);
+gather(filter, PidElementList) ->
+    filter_gather(PidElementList).
 
 -spec map_gather([pid()]) -> [any()].
-map_gather([Pid | Rest]) ->
+map_gather([{Pid, _E} | Rest]) ->
     receive
-	% I am torn on the timeout. I am not sure it should be added
-	% into the return list. If a process does not return a value
-	% perhaps it should be excluded from the results. A map
-	% presumes a map across all elements. If one does not work
-	% adding in 'timeout' as a substitute is not strictly correct.
-	% There are two modes that seem valuable, asyncronous, excluding timeout
-	% or atomic/syncronous such that all operations must succeed to return
-	% an application level result or the whole operation fails. Take alook
-	% at filter, it treats timeout like false, which is not quite congruent
-	% with how map treats it. Why not treat timeout like true and include
-	% timeout in the list?
-        {Pid, {normal, Ret}} -> [Ret|map_gather(Rest)];
-        {Pid, {exception, {_Class, Ex}}} -> throw(Ex);
-        {Pid, timeout = Ret} -> [Ret|map_gather(Rest)]
+        {Pid, {value, Ret}} ->
+	    [Ret|map_gather(Rest)];
+	% timeouts fall here too. Should timeouts be a return value
+	% or an exception? I lean toward return value, but the code
+	% is easier with the exception. Thoughts?
+        {Pid, Exception} ->
+	    killall(Rest),
+	    throw(Exception) 
     end;
 map_gather([]) ->
     [].
 
--spec filter_gather([pid()]) -> [any()].
-filter_gather([Pid | Rest]) ->
+-spec ftmap_gather([pid()]) -> [any()].
+ftmap_gather([{Pid, _E} | Rest]) ->
     receive
-        {Pid, {normal, false}} -> filter_gather(Rest);
-        {Pid, {normal, Ret}} -> [Ret|filter_gather(Rest)];
-        {Pid, {exception, {_Class, Ex}}} -> throw(Ex);
-        {Pid, timeout} -> filter_gather(Rest)
+        {Pid, Value} -> [Value|ftmap_gather(Rest)]
+    end;
+ftmap_gather([]) ->
+    [].
+
+-spec filter_gather([pid()]) -> [any()].
+filter_gather([{Pid, E} | Rest]) ->
+    receive
+        {Pid, {value, false}} ->
+	    filter_gather(Rest);
+        {Pid, {value, true}} ->
+	    [E|filter_gather(Rest)];
+        {Pid, {value, NotBool}} ->
+	    killall(Rest),
+	    throw({bad_return_value, NotBool});
+        {Pid, Exception} ->
+	    killall(Rest),
+	    throw(Exception)
     end;
 filter_gather([]) ->
     [].
 
 -spec do_f(pid(), fun(), any())  -> no_return().
-do_f(Parent, F, I) ->
+do_f(Parent, F, E) ->
     try
-        Result = F(I),
-	% The protocol has the format {Parent::pid(), Result}
-	% where
-	%  Result = {ResultClass, Result} 
-	%   ResultClass = normal | exception
-	%   Result = term()
-        Parent ! {self(), {normal, Result}}
+        Result = F(E),
+        Parent ! {self(), {value, Result}}
     catch
-        Class:Exception ->
-            Parent ! {self(), {exception, {Class, Exception}}}
+        _Class:Exception ->
+	    % Losing class info here, but since throw does not accept
+	    % that arg anyhow and forces a class of throw it does not
+	    % matter.
+            Parent ! {self(), Exception}
     end.
+
+-spec killall([pid()]) -> ok.
+killall([{Pid, _E}|T]) ->
+    exit(Pid, kill),
+    killall(T);
+killall([]) ->
+    ok.
 
 %%=============================================================================
 %% Tests
 %%=============================================================================
-pmap_good_test() ->
+map_good_test() ->
     Results = map(fun(_) ->
                           ok
                   end,
@@ -143,40 +175,75 @@ pmap_good_test() ->
     ?assertMatch([ok, ok, ok, ok, ok],
                  Results).
 
-pfilter_good_test() ->
-    Results = filter(fun(X) ->
-                             X
-                     end,
-                     [true, false, true], infinity),
-    ?assertMatch([true, true],
+ftmap_good_test() ->
+    Results = ftmap(fun(_) ->
+                          ok
+                  end,
+                 lists:seq(1, 3), infinity),
+    ?assertMatch([{value, ok}, {value, ok}, {value, ok}],
                  Results).
 
-pfilter_timeout_test() ->
+filter_good_test() ->
     Results = filter(fun(X) ->
-                             timer:sleep(X),
-                             true
+                             X == show
                      end,
-                     [100, 1], 10),
-    ?assertMatch([true],
+                     [show, show, remove], infinity),
+    ?assertMatch([show, show],
                  Results).
 
-pmap_timeout_test() ->
-    Results = map(fun(X) ->
+map_timeout_test() ->
+    Results =
+	try 
+	    map(fun(T) ->
+			timer:sleep(T),
+			T
+		end,
+		[1, 100], 10)
+	catch
+	    C:E -> {C, E}
+	end,
+    ?assertMatch({throw, timeout}, Results).
+
+ftmap_timeout_test() ->
+    Results = ftmap(fun(X) ->
                           timer:sleep(X),
                           true
                   end,
                   [100, 1], 10),
-    ?assertMatch([timeout, true],
-                 Results).
+    ?assertMatch([timeout, {value, true}], Results).
 
-pmap_bad_test() ->
+filter_timeout_test() ->
+    Results =
+	try 
+	    filter(fun(T) ->
+			   timer:sleep(T),
+			   T == 1
+		   end,
+		   [1, 100], 10)
+	catch
+	    C:E -> {C, E}
+	end,
+    ?assertMatch({throw, timeout}, Results).
+
+map_bad_test() ->
     Results =
 	try 
 	    map(fun(_) ->
-			erlang:throw(test_exception)
+			throw(test_exception)
 		end,
 		lists:seq(1, 5), infinity)
 	catch
 	    C:E -> {C, E}
 	end,
     ?assertMatch({throw, test_exception}, Results).
+
+ftmap_bad_test() ->
+    Results =
+	ftmap(fun(2) ->
+		      throw(test_exception);
+		 (N) -> 
+		      N 
+	      end,
+	      lists:seq(1, 5), infinity),
+    ?assertMatch([{value, 1}, test_exception, {value, 3}, {value, 4}, {value, 5}] , Results).
+
